49fc093075b2c3a81dd6bd915d0756fa456c08d5
Yamaguchi-taichi <yamaguchi-o20@route-zero.com>
2023-11-14 09:30:11 +0900

    earthモデルレスポンシブ対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,126 @@
-// prettier-ignore
-"use client"
-import Image from 'next/image'
-import { useEffect } from 'react'
+'use client'
+import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 export default function Home() {
+  const gltfLoader: GLTFLoader = new GLTFLoader()
+  let mixer: THREE.AnimationMixer
+  let model: THREE.Group
+  let camera: THREE.PerspectiveCamera
   let canvas: HTMLElement
+  let fov: number
+  let animationId: number
+
   useEffect(() => {
-    if (canvas) return
-    canvas = document.getElementById('canvas')!
-    //シーン
+    // canvas取得
+    if (typeof window === 'object') {
+      canvas = document.getElementById('canvas')!
+    }
+    //シーン初期化
     const scene = new THREE.Scene()
-    //カメラ
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    )
-    //レンダラー
+    //レンダラー初期化
     const renderer = new THREE.WebGLRenderer({
       canvas: canvas || undefined,
-      antialias: true,
+      antialias: false,
       alpha: true,
     })
     renderer.setSize(window.innerWidth, window.innerHeight)
     renderer.setPixelRatio(window.devicePixelRatio)
-    // document.body.appendChild(renderer.domElement)
 
-    //立方体作成
-    const geometory = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    const cube = new THREE.Mesh(geometory, material)
-    cube.position.z = -5
-    cube.rotation.set(10, 10, 10)
-    scene.add(cube)
+    // リサイズ処理
+    const onResize = () => {
+      // modelセット位置(Z・Y軸)レスポンシブ対応
+      let modelPositionY: number
+      let modelPositionZ: number
+      // アスペクト比率
+      let aspectRatio: number = window.innerWidth / window.innerHeight
+      // カメラの角度・モデルセット位置(レスポンシブ対応)
+      if (aspectRatio > 1.7) {
+        fov = 75
+        modelPositionY = 0.1
+        modelPositionZ = -0.9
+      } else if (aspectRatio > 1.1) {
+        fov = 75
+        modelPositionY = 0.15
+        modelPositionZ = -0.9
+      } else if (aspectRatio > 1) {
+        fov = 75
+        modelPositionY = 0
+        modelPositionZ = 0
+      } else if (aspectRatio > 0.7) {
+        fov = 75
+        modelPositionY = 0.3
+        modelPositionZ = -1.5
+      } else if (aspectRatio > 0.6) {
+        fov = 75
+        modelPositionY = 0
+        modelPositionZ = -1.5
+      } else {
+        fov = 100
+        modelPositionY = 1.2
+        modelPositionZ = -2
+      }
+      // 3Dモデルインポート
+      gltfLoader.load('./models/scene.gltf', (gltf) => {
+        model = gltf.scene
+        model.scale.set(0.003, 0.003, 0.003)
+        model.rotation.x = Math.PI / 2
+        model.position.set(0, modelPositionY, modelPositionZ)
+        scene.add(model)
 
-    //ライト
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
-    scene.add(ambientLight)
-    const pointLight = new THREE.PointLight(0xffffff, 0.2)
-    pointLight.position.set(1, 2, 3)
-    scene.add(pointLight)
+        mixer = new THREE.AnimationMixer(model)
+        const clips = gltf.animations
+        clips.forEach((clips) => {
+          const action = mixer.clipAction(clips)
+          action.play()
+        })
+      })
+      console.log('リサイズ')
+      // サイズを取得
+      const width = window.innerWidth
+      const height = window.innerHeight
+
+      // カメラ初期化
+      camera = new THREE.PerspectiveCamera(fov, aspectRatio, 0.1, 10)
+      camera.position.set(0, 0, 0)
+
+      // レンダラーのサイズを調整する
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(width, height)
+
+      // カメラのアスペクト比を正す
+      camera.aspect = width / height
+      camera.updateProjectionMatrix()
+    }
+
+    // 初期化のため実行
+    onResize()
+    window.addEventListener('resize', onResize)
+
+    //const clock = new THREE.Clock()
 
     //アニメーション
-    const clock = new THREE.Clock()
     const animation = () => {
-      const elapsedTime = clock.getElapsedTime()
-      cube.rotation.x = elapsedTime
-      cube.rotation.y = elapsedTime
-      window.requestAnimationFrame(animation)
+      // PCの処理性能によって時間変わるため各ローカルで時間取得処理したい
+      // const elapsedTime = clock.getElapsedTime()
       renderer.render(scene, camera)
+      if (mixer) {
+        mixer.update(0.01)
+      }
+      animationId = requestAnimationFrame(animation)
     }
     animation()
+
+    // クリーンアップ関数
+    return () => {
+      window.removeEventListener('resize', onResize)
+      cancelAnimationFrame(animationId)
+    }
   }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex w-full h-full min-h-screen flex-col items-center justify-between overflow-hidden">
       <canvas id="canvas"></canvas>
     </main>
   )
